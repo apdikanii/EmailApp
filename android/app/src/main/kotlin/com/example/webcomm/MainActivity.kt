29c2121c14d4f5eadd826fe95c18901ada396d98
abdikani <abdikanilibaxa>
2023-01-19 14:23:39 +0300

    update

---
+++ android/app/src/main/kotlin/com/example/webcomm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webcomm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/cawaaleict/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/cawaaleict/Desktop/webcomm" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/cawaaleict/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/cawaaleict/Desktop/webcomm"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/cawaaleict/Desktop/webcomm/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/cawaaleict/Desktop/webcomm/lib/main.dart"
+)
